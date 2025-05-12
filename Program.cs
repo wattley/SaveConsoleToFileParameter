40c68944a50095d777dbb337e4d0475c5b2f98b0
Jan van der Watt <janvanderwatt@gmail.com>
2025-05-12 12:41:23 +1000

    Added ability to wrap log file daily.

--- Program.cs
+++ Program.cs
@@ -8,50 +8,29 @@ class Program
 {
     [DllImport("kernel32.dll", SetLastError = true)] static extern bool SetConsoleTitle(string lpConsoleTitle);
 
-    static void Main(string[] args)
+    static bool keepRunning = true;
+    const String FILE_DATE_TIME_FORMAT = "yyMMddHHmm";
+    const String FILE_DATE_FORMAT = "yyMMdd";
+    const String LOG_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    static string fileSpec = "", filePath = "";
+    static string portName = "";
+    static int baudRate = 0;
+    static bool wrapDaily = false;
+    static DateTime today;
+
+    static void checkEscapeKey()
     {
-        //string filePath = @"C:\Users\Jan\LightCat Dropbox\Projects\Trevor\VoidDetect\Calculations and Data\20241223 Console Output.txt";
-        //string portName = "COM5"; // Replace with your port name
-        //int baudRate = 115200; // Replace with your baud rate
-        string filePath = "";
-        string portName = "";
-        int baudRate = 0;
-        int haveParameterFlags = 0;
-        const int HAVE_PORT = 1, HAVE_FILE_PATH = 2, HAVE_BAUD_RATE = 4;
-        SerialPort serialPort = null;
-        const String FILE_DATE_TIME_FORMAT = "yyMMddHHmm";
-        const String LOG_DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
-
-        // Iterate through the arguments
-        for (int i = 0; i < args.Length; i++)
+        if (Console.KeyAvailable)
         {
-            if (args[i] == "-p" && i + 1 < args.Length)
-            {
-                portName = args[i + 1];
-                haveParameterFlags |= HAVE_PORT;
-            }
-            else if (args[i] == "-f" && i + 1 < args.Length)
-            {
-                filePath = args[i + 1];
-                haveParameterFlags |= HAVE_FILE_PATH;
-            }
-            else if (args[i] == "-b" && i + 1 < args.Length)
-            {
-                Int32.TryParse(args[i + 1], out baudRate);
-                haveParameterFlags |= HAVE_BAUD_RATE;
-            }
+            ConsoleKey key = Console.ReadKey(intercept: true).Key;
+            keepRunning = (key != ConsoleKey.Escape);
         }
+    }
 
-        if (haveParameterFlags != (HAVE_PORT + HAVE_FILE_PATH + HAVE_BAUD_RATE))
-        {
-            Console.WriteLine("Usage:");
-            Console.WriteLine($"\t-f <LOG FILE PATH, put [DATETIME] in filename for it to be replaced by {FILE_DATE_TIME_FORMAT}>");
-            Console.WriteLine("\t-p <COM PORT>");
-            Console.WriteLine("\t-b <BAUD RATE>");
-            return;
-        }
-
-        filePath = filePath.Replace("[DATETIME]", DateTime.Now.ToString(FILE_DATE_TIME_FORMAT));
+    static void MakeLogFileIfNotExists()
+    {
+        today = DateTime.Now;
+        filePath = fileSpec.Replace("[DATETIME]", today.ToString(wrapDaily ? FILE_DATE_FORMAT : FILE_DATE_TIME_FORMAT));
 
         string title = portName + "->" + filePath;
         SetConsoleTitle(title);
@@ -77,102 +56,181 @@ class Program
                 writer.Flush(); // Ensure data is written to the file immediately
             }
         }
+    }
 
-        Boolean keepRunning = true;
-        int retryCount = 2 * 30;    // Try for 30 seconds
-        Console.Write($"Opening {portName} ");
-        while (serialPort == null && retryCount > 0)
+    static void Main(string[] args)
+    {
+        //string filePath = @"C:\Users\Jan\LightCat Dropbox\Projects\Trevor\VoidDetect\Calculations and Data\20241223 Console Output.txt";
+        //string portName = "COM5"; // Replace with your port name
+        //int baudRate = 115200; // Replace with your baud rate
+        int haveParameterFlags = 0;
+        const int HAVE_PORT = 1, HAVE_FILE_PATH = 2, HAVE_BAUD_RATE = 4;
+        SerialPort serialPort = null;
+        string previousMessage = "";
+
+        // Iterate through the arguments
+        for (int i = 0; i < args.Length; i++)
         {
-            Console.Write("_");
-            try
+            if (args[i] == "-p" && i + 1 < args.Length)
             {
-                // Open the serial port
-                serialPort = new SerialPort(portName, baudRate);
-                serialPort.Open();
+                portName = args[i + 1];
+                haveParameterFlags |= HAVE_PORT;
             }
-            catch (Exception ex)
+            else if (args[i] == "-f" && i + 1 < args.Length)
             {
-                // Console.WriteLine($"Error: {ex.Message}");
-                serialPort.Dispose();
-                serialPort = null;
-                retryCount--;
-                System.Threading.Thread.Sleep(500);
+                fileSpec = args[i + 1];
+                haveParameterFlags |= HAVE_FILE_PATH;
+            }
+            else if (args[i] == "-b" && i + 1 < args.Length)
+            {
+                Int32.TryParse(args[i + 1], out baudRate);
+                haveParameterFlags |= HAVE_BAUD_RATE;
+            }
+            else if (args[i] == "-d")
+            {
+                wrapDaily = true;
             }
         }
 
-        if (serialPort == null)
+        bool misconfigured = haveParameterFlags != (HAVE_PORT + HAVE_FILE_PATH + HAVE_BAUD_RATE);
+        if (wrapDaily && !fileSpec.Contains("[DATETIME]"))
         {
-            Console.WriteLine("Error: Could not open serial port");
+            Console.WriteLine("ERROR: Wrap daily option requires [DATETIME] in the file path.\n");
+            misconfigured = true;
+        }
+
+        if (misconfigured)
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine($"\t-f <LOG FILE PATH, put [DATETIME] in filename for it to be replaced by {FILE_DATE_TIME_FORMAT}>");
+            Console.WriteLine("\t-p <COM PORT>");
+            Console.WriteLine("\t-b <BAUD RATE>");
+            Console.WriteLine($"\t-d\tWrap log file daily, [DATETIME] in filename replaced by {FILE_DATE_FORMAT}>");
             return;
         }
-        else
-        {
-            Console.WriteLine(" READY!");
-        }
 
-        // Create a SerialPort instance
-        using (serialPort)
+        MakeLogFileIfNotExists();
+
+        while (keepRunning)
         {
-            try
+            Console.Write($"Opening {portName} ");
+            while (serialPort == null && keepRunning)
             {
-                Console.WriteLine($"Reading data from {portName}...");
-                serialPort.ReadTimeout = 500;
-
-                Boolean write_time = false;
-                String text = "";
-
-                // Continuously read data
-                while (keepRunning)
+                Console.Write("_");
+                try
                 {
-                    if (Console.KeyAvailable)
-                    {
-                        ConsoleKey key = Console.ReadKey(intercept: true).Key;
-                        keepRunning = (key != ConsoleKey.Escape);
-                    }
-                    // Read a line of data from the serial port
-                    try
-                    {
-                        int data = serialPort.ReadChar();
-                        char c = System.Convert.ToChar(data);
-
-                        if (write_time && (c != '\r') && (c != '\n'))
-                        {
-                            write_time = false;
-                            DateTime now = DateTime.Now;
-                            String now_text = now.ToString(LOG_DATE_TIME_FORMAT) + ": ";
-                            Console.Write(now_text);
-                            text = now_text;
-                        }
-
-                        // Display the data on the console
-                        Console.Write(c);
-
-                        // Write the data to the file
-                        text += c;
-
-                        if ((c == '\n') || (c == '\r'))
-                        {
-                            // Append to the file with shared access
-                            using (FileStream fs = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
-                            using (StreamWriter writer = new StreamWriter(fs))
-                            {
-                                writer.Write(text);
-                                writer.Flush(); // Ensure data is written to the file immediately
-                                text = "";
-                            }
-                            write_time = true;
-                        }
-                    }
-                    catch (TimeoutException)
-                    {
-                        // Ignore timeout exceptions
-                    }
+                    // Open the serial port
+                    serialPort = new SerialPort(portName, baudRate);
+                    serialPort.Open();
                 }
+                catch /*(Exception ex)*/
+                {
+                    // Console.WriteLine($"Error: {ex.Message}");
+                    serialPort.Dispose();
+                    serialPort = null;
+                    System.Threading.Thread.Sleep(500);
+                }
+                checkEscapeKey();
             }
-            catch (Exception ex)
+
+            if (!keepRunning)
             {
-                Console.WriteLine($"Error: {ex.Message}");
+                return;
             }
-        }
+
+            Console.WriteLine(" READY!");
+
+            // Create a SerialPort instance
+            using (serialPort)
+            {
+                try
+                {
+                    Console.WriteLine($"Reading data from {portName}...");
+                    serialPort.ReadTimeout = 500;
+
+                    Boolean write_time = false;
+                    String text = "";
+
+                    // Continuously read data
+                    while (keepRunning)
+                    {
+                        checkEscapeKey();
+                        // Read a line of data from the serial port
+                        try
+                        {
+                            int data = serialPort.ReadChar();
+                            char c = System.Convert.ToChar(data);
+                            DateTime now = DateTime.Now;
+
+                            if (write_time && (c != '\r') && (c != '\n'))
+                            {
+                                write_time = false;
+                                String now_text = now.ToString(LOG_DATE_TIME_FORMAT) + ": ";
+                                Console.Write(now_text);
+                                text += now_text;
+                            }
+
+                            // Display the data on the console
+                            Console.Write(c);
+
+                            // Write the data to the file
+                            text += c;
+
+                            if ((c == '\n') || (c == '\r'))
+                            {
+                                // Append to the file with shared access
+                                try
+                                {
+                                    // If the user has specified to wrap the log file daily, check if the date has changed
+                                    if (wrapDaily && (today.ToString(FILE_DATE_FORMAT) != now.ToString(FILE_DATE_FORMAT)))
+                                    {
+                                        Console.WriteLine("--> Wrapping into a new log file for the day.");
+                                        // Create the new log file
+                                        MakeLogFileIfNotExists();
+                                    }
+                                    String todayText = DateTime.Now.ToString(FILE_DATE_FORMAT);
+                                    using (FileStream fs = new FileStream(filePath, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+                                    {
+                                        using (StreamWriter writer = new StreamWriter(fs))
+                                        {
+                                            writer.Write(text);
+                                            writer.Flush(); // Ensure data is written to the file immediately
+                                            text = "";
+                                        }
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.WriteLine($"Error: {ex.Message}");
+                                    Console.WriteLine(" --- will try again later");
+                                }
+                                write_time = true;
+                            }
+                        }
+                        catch (TimeoutException)
+                        {
+                            // Ignore timeout exceptions
+                        }
+                        catch (Exception ex)
+                        {
+                            if (previousMessage != ex.Message)
+                            {
+                                Console.WriteLine($"Error: {ex.Message}");
+                                previousMessage = ex.Message;
+                            }
+                        }
+                        if (!serialPort.IsOpen)
+                        {
+                            break;
+                        }
+                    }   // while (keepRunning)
+                }   // try
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error: {ex.Message}");
+                }   // catch
+            }   // using (serialPort)
+            serialPort = null;
+        }   // while (keepRunning)
     }
 }
